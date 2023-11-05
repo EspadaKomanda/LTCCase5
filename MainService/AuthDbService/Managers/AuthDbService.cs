178f1e9362670bfe4a96afa22c6be19318f0afa7
FerrusManus <roma.hlinin@yandex.ru>
2023-11-05 11:56:01 +0300

    Fixing Errors

--- MainService/AuthDbService/Managers/AuthDbService.cs
+++ MainService/AuthDbService/Managers/AuthDbService.cs
@@ -1,6 +1,7 @@
 ﻿using AuthDbService.Database;
 using AuthDbService.Database.Models;
 using Grpc.Core;
+using System.ComponentModel.DataAnnotations;
 
 namespace AuthDbService.Managers
 {
@@ -8,40 +9,50 @@ namespace AuthDbService.Managers
     {
         public async Task<string> AddUser(UserModel request)
         {
-            try
+            string ValidationResult = Validate(request);
+            if (ValidationResult=="")
             {
-                using (ApplicationContext ctx = new ApplicationContext())
+                try
                 {
-                    var state = await ctx.users.AddAsync(new UserModel()
+                    using (ApplicationContext ctx = new ApplicationContext())
                     {
-                        uuid = request.uuid,
-                        email = request.email,
-                        firstName = request.firstName,
-                        lastName = request.lastName,
-                        patronymic = request.patronymic,
-                        password = request.password,
-                        role = request.role
-                    });
-                    ctx.SaveChanges();
-                    if (state.State == Microsoft.EntityFrameworkCore.EntityState.Added)
-                    {
-                        return "";
+
+                        var state = await ctx.users.AddAsync(new UserModel()
+                        {
+                            uuid = Guid.NewGuid(),
+                            email = request.email,
+                            firstName = request.firstName,
+                            lastName = request.lastName,
+                            patronymic = request.patronymic,
+                            password = request.password,
+                            role = request.role
+                        });
+                        await ctx.SaveChangesAsync();
+                        if (state.State == Microsoft.EntityFrameworkCore.EntityState.Added)
+                        {
+                            return "Added user successfully";
+                        }
+                        //Cringe but we wanted a string here
+                        return "EntityState.Added is false";
                     }
-                    //Cringe but we wanted a string here
-                    return "EntityState.Added is false";
+                }
+                catch (Exception ex)
+                {
+                    return ex.ToString();
                 }
             }
-            catch (Exception ex) 
+            else
             {
-                return ex.ToString();
+                return ValidationResult;
             }
+          
         }
 
-        public async Task<string> DeleteUser(UserModel request)
+        public async Task<string> DeleteUserByEmail(UserModel model)
         {
             using (ApplicationContext ctx = new ApplicationContext())
             {
-                var user = await ctx.users.FindAsync(request.uuid);
+                var user =  ctx.users.Where(p => p.email == model.email).ToList()[0];
                 if (user != null)
                 {
                     ctx.users.Remove(user);
@@ -49,11 +60,48 @@ namespace AuthDbService.Managers
                     return "";
                 }
 
-                return String.Format("User ({0} does not exist", request.uuid);
+                return String.Format("User ({0} does not exist", model.email);
             }
         }
 
-        public async Task<string> ModifyUser(UserModel request)
+        public async Task<string> ModifyUserByEmail(UserModel request)
+        {
+            using (ApplicationContext ctx = new ApplicationContext())
+            {
+                var user = ctx.users.Where(p => p.email == request.email).ToList()[0];
+                if (user != null)
+                {
+                    user.email = request.email ?? user.email;
+                    user.firstName = request.firstName ?? user.firstName;
+                    user.lastName = request.lastName ?? user.lastName;
+                    user.patronymic = request.patronymic ?? user.patronymic;
+                    user.password = request.password ?? user.password;
+                    user.role = request.role ?? user.role;
+
+                    await ctx.SaveChangesAsync();
+                    return "";
+                }
+
+                return $"User ({request.email} does not exist";
+            }
+        }
+        public async Task<string> DeleteUserById(UserModel model)
+        {
+            using (ApplicationContext ctx = new ApplicationContext())
+            {
+                var user = await ctx.users.FindAsync(model.uuid);
+                if (user != null)
+                {
+                    ctx.users.Remove(user);
+                    await ctx.SaveChangesAsync();
+                    return "";
+                }
+
+                return String.Format("User ({0} does not exist", model.email);
+            }
+        }
+
+        public async Task<string> ModifyUserById(UserModel request)
         {
             using (ApplicationContext ctx = new ApplicationContext())
             {
@@ -71,11 +119,10 @@ namespace AuthDbService.Managers
                     return "";
                 }
 
-                return String.Format("User ({0} does not exist", request.uuid);
+                return $"User ({request.email} does not exist";
             }
         }
-
-        public async Task<UserModel> GetUser(UserModel request)
+        public async Task<UserModel> GetUserById(UserModel request)
         {
             var user = new UserModel();
             using (ApplicationContext ctx = new ApplicationContext())
@@ -83,24 +130,46 @@ namespace AuthDbService.Managers
                 user = await ctx.users.FindAsync(request.uuid);
             }
 
-            //I don't know what this is for 
-            if (user.firstName == "unk")
+
+            if (user.firstName == null)
             {
                 return await Task.FromResult(new UserModel());
-
             }
 
-            return await Task.FromResult(new UserModel()
-            {
-                uuid = request.uuid,
-                email = request.email,
-                firstName = request.firstName,
-                lastName = request.lastName,
-                patronymic = request.patronymic,
-                password = request.password,
-                role = request.role
-            });
+            return await Task.FromResult(user);
 
         }
+        public async Task<UserModel> GetUserByEmail(UserModel request)
+        {
+            var user = new UserModel();
+            using (ApplicationContext ctx = new ApplicationContext())
+            {
+                user = await ctx.users.FindAsync(request.uuid);
+            }
+
+            if (user.firstName == null)
+            {
+                return await Task.FromResult(new UserModel());
+            }
+
+            return await Task.FromResult(user);
+
+        }
+        private string Validate(UserModel user)
+        {
+            var results = new List<ValidationResult>();
+            var context = new ValidationContext(user);
+            string errors = "";
+            if (!Validator.TryValidateObject(user, context, results, true))
+            {
+                foreach (var error in results)
+                {
+                    errors+=error.ErrorMessage+'\n';
+                }
+            }
+
+            return errors;
+        }
+
     }
 }
