504fb3990829501124c044c06048c6a50ee28eb2
FerrusManus <roma.hlinin@yandex.ru>
2023-11-07 20:35:22 +0300

    And another one

--- MainService/MainService/Pages/razorPages/Profile.cshtml.cs
+++ MainService/MainService/Pages/razorPages/Profile.cshtml.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MainService.Pages.razorPages
 {
+    [Authorize]
     public class ProfileModel : PageModel
     {
         public void OnGet()
